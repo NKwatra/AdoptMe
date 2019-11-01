863106cb42369a0d64de53804884d3f5ece4b1fc
NKwatra <kwatranishkarsh@gmail.com>
2019-11-01 16:20:11 +0530

    Use react hooks and create custom hook

---
+++ src/SearchParams.js
@@ -0,0 +1,29 @@
+import React, { useState } from "react";
+import { ANIMALS } from "@frontendmasters/pet";
+import useDropdown from "./useDropdown";
+const SearchParams = () => {
+  const [location, setLocation] = useState("Seattle, WA");
+  const [breeds, setBreeds] = useState([]);
+  const [animal, AnimalDropdown] = useDropdown("animal", "dog", ANIMALS);
+  const [breed, BreedDropdown] = useDropdown("breed", "", breeds);
+  return (
+    <div className="search-params">
+      <form>
+        <label htmlFor="location">
+          Location
+          <input
+            id="location"
+            value={location}
+            placeholder="Location"
+            onChange={e => setLocation(e.target.value)}
+          />
+        </label>
+        <AnimalDropdown />
+        <BreedDropdown />
+        <button>Submit</button>
+      </form>
+    </div>
+  );
+};
+
+export default SearchParams;
